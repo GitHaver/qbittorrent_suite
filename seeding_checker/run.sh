6c56dbad6b05f159f9cce222a45c3500fd9e0eff
dylan <dylan_cowie@outlook.com.au>
2023-07-16 19:03:31 +1000

    Updated run.sh to run from parent dir to import consistently. Also updating to log to the seeding_checker folder instead of a log subfolder, as it's only 1 log file this is cleaner.

--- seeding_checker/run.sh
+++ seeding_checker/run.sh
@@ -1,3 +1,3 @@
-source ../venv/bin/activate
-mkdir -p log
-python3 seeding_checker.py >> log/seeding_checker.log 2>&1
+cd ..
+source venv/bin/activate
+python3 seeding_checker/seeding_checker.py >> seeding_checker/seeding_checker.log 2>&1
